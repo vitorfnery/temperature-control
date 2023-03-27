8d7db3d7964e0cc36944d3f3bac143e0c8c53242
Vítor F. Nery <vitorfelipenery@gmail.com>
2023-03-27 12:57:14 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Temperature Control
+
+A temperature control that allows the user to increase or decrease the temperature value (in ºC).
+If the temperature has a value above 15ºC, the background turns orange. Else, the background is blue.
+Made with React.
+Available on (Temperature Control - Vítor F. Nery)[https://temperature-control-vitorfnery.netlify.app/] 
---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const App = () => {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+
+  const increaseTemperature = () => {
+    if (temperatureValue === 30) return;
+    const newTemperature = temperatureValue + 1;
+    newTemperature >= 15 && setTemperatureColor("hot");
+    setTemperatureValue(newTemperature);
+  };
+
+  const decreaseTemperature = () => {
+    if (temperatureValue === 0) return;
+    const newTemperature = temperatureValue - 1;
+    newTemperature < 15 && setTemperatureColor("cold");
+    setTemperatureValue(newTemperature);
+  };
+
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${temperatureColor}`}>
+          {temperatureValue}ºC
+        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={() => increaseTemperature()}>+</button>
+        <button onClick={() => decreaseTemperature()}>-</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
